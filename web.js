84fcdc69f88f867702ab3293dd90cbcd73af4b18
LoaCos83 <67106370+LoaCos83@users.noreply.github.com>
2023-04-04 14:09:39 +0200

    CONNECTION !!!! EXPLOSION !!!!

--- web.js
+++ web.js
@@ -1,52 +1,86 @@
-src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.1.0/paho-mqtt.min.js" ; integrity="sha384-7MzBrHzx0xIKZd8H4Jok4qgC0JLzNjHk25r9YrMbNnqjQhdJGdce/ECWTfPn/BwN" ; crossorigin="anonymous"
-        // Connexion au broker MQTT
-	    var client = new Paho.MQTT.Client("broker.mqttdashboard.com", Number(1883), "clientId");
+var mqtt;
 
-		client.connect({
-            onSuccess: function () {
-                console.log("Connecté au broker MQTT !");
-                // Publication du message sur le topic "isen01"
-                var message = new Paho.MQTT.Message("Hello World !");
-                message.destinationName = "isen01";
-                client.send(message);
-                console.log("Message publié sur le topic isen01 !");
-            },
-            onFailure: function (err) {
-                console.error("Erreur de connexion au broker MQTT : " + err);
-            }
-        });
+        var reconnectTimeout = 2000;
 
-		// Fonction pour publier un message sur un topic
-		function publierMessage(topic, message) {
-			var message = new Paho.MQTT.Message(message);
-			message.destinationName = topic;
-			client.send(message);
-			console.log("Message envoyé sur le topic " + topic + ": " + message.payloadString);
-		}
+        //var host="192.168.1.163"; //change this
+
+        //var host="82.165.158.236";
+
+        //var host="steve-laptop"; //change this
+
+        var host="broker.mqttdashboard.com";
+
+        var port=8000
+
+        //var port=9001;
+
+        //var port=8881;
+
+       
+
+        function onFailure(message) {
+
+            console.log("Connection Attempt to Host "+host+"Failed");
+
+            setTimeout(MQTTconnect, reconnectTimeout);
+
+        }
+
+        function onMessageArrived(msg){
+
+            out_msg="Message received "+msg.payloadString+"<br>";
+
+            out_msg=out_msg+"Message received Topic "+msg.destinationName;
+
+            console.log(out_msg);
 
 
-// Compteur de clics
-var countButton = document.getElementById("countButton");
-var count = document.getElementById("count");
-var clickCount = 0;
-countButton.addEventListener("click", function() {
-    clickCount++;
-    count.innerHTML = clickCount;
-    });
 
-// Contrôle de la LED
-var redButton = document.querySelector(".red");
-var greenButton = document.querySelector(".green");
-var blueButton = document.querySelector(".blue");
-redButton.addEventListener("click", function() {
-    // Envoyer une commande à la carte STM32 pour allumer la LED rouge
-    });
-greenButton.addEventListener("click", function() {
-    // Envoyer une commande à la carte STM32 pour allumer la LED verte
-    });
-blueButton.addEventListener("click", function() {
-    // Envoyer une commande à la carte STM32 pour allumer la LED bleue
-    });
 
-// Affichage de la courbe de température
-// Utiliser une bibliothèque de graphiques comme Chart.js pour afficher la courbe de température
+        }
+
+        function onConnect() {
+
+            console.log("Connected ");
+
+            mqtt.subscribe("isen01/led");
+
+            var stringMessage = "{\"id\": 1,\"state\": 1}";
+
+            message = new Paho.MQTT.Message(stringMessage.toString());
+
+            message.destinationName = "isen01/led";
+
+            message.retained=true;
+
+            mqtt.send(message);
+
+      }
+
+      function MQTTconnect() {
+
+        console.log("connecting to "+ host +" "+ port);
+
+            var x=Math.floor(Math.random() * 10000);
+
+        var cname="orderform-"+x;
+
+        mqtt = new Paho.MQTT.Client(host,port,cname);
+
+        //document.write("connecting to "+ host);
+
+        var options = {
+
+            timeout: 3,
+
+            onSuccess: onConnect,
+
+            onFailure: onFailure,
+
+             };
+
+         mqtt.onMessageArrived = onMessageArrived
+
+         mqtt.connect(options); //connect
+
+        }
